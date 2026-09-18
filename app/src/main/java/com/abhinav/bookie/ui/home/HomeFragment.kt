7c93e25c61f782236f538a73cdf13d46712b3e5a
Abhinav-engg <abhinav.pal6.7.9@gmail.com>
2026-09-18 18:59:46 +0530

    feat: added basic layout for activty_main

---
+++ app/src/main/java/com/abhinav/bookie/ui/home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.abhinav.bookie.ui.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.abhinav.bookie.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
